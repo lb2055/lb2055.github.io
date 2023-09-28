4685b2fd300438243091e2337a69b313eb9394cd
lb2055 <145141031+lb2055@users.noreply.github.com>
2023-09-28 09:13:09 +0100

    added javascript

---
+++ testJavaScript.js
@@ -0,0 +1,18 @@
+document.body.style.height = '200pt';
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js';
+script.id = 'cookiebanner';
+script['data-height']   = '20px';
+script['data-position'] = 'top';
+script['data-message']  = 'We use cookies!';
+document.head.appendChild(script); 
+
+script.onload = function()
+{
+  console.log('loaded script');
+
+  // console.log( initCookieConsent );
+}
+console.log('ready');
